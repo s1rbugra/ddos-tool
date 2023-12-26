af59d365c13b9930a55bb77996a1ea143c556e96
s1rbugra <152310633+s1rbugra@users.noreply.github.com>
2023-12-26 22:48:14 +0300

    app.py

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
-#%% banner
 banner = """
-##########################
-#      DDOS TOOL         #
-#      s1r.bugra         #
-#       GİTHUB           #
-#      s1r.bugra         #
-#      DDOS TOOL 4.0     #
-##########################
+###################
+# DDOS TOOL 4.0   #
+#    GİTHUB       #
+#   s1r.bugra     #
+###################
 """
 print(banner)
-#%% DEĞİŞKENLER
+ip_giriş = input("DDOS ATILACAK İP GİRİN: ")
+port_giriş = int(input("PORTU GİRİN: "))
+sayi_giriş = int(input("DDOS ATILACAK SAYIYI GİRİN: "))
+print("ATAK BAŞLATILIYOR")
+for i in range(sayi_giriş):
+  print(i)
+print("ATAK BAŞARILI")
